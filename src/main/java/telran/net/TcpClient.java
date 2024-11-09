8d748aa58422d30d81169bf8d2d56e4a3b74a3e7
yanfi-sher <loss.soul20@gmail.com>
2024-11-09 11:54:10 +0200

    updates alternative flows of server unavailability

--- src/main/java/telran/net/TcpClient.java
+++ src/main/java/telran/net/TcpClient.java
@@ -1,12 +1,20 @@
 package telran.net;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
 import java.time.Instant;
 
 import org.json.JSONObject;
 
-import static telran.net.TcpConfigurationProperties.*;
+import static telran.net.TcpConfigurationProperties.DEFAULT_INTERVAL_CONNECTION;
+import static telran.net.TcpConfigurationProperties.DEFAULT_TRIALS_NUMBER_CONNECTION;
+import static telran.net.TcpConfigurationProperties.RESPONSE_CODE_FIELD;
+import static telran.net.TcpConfigurationProperties.RESPONSE_DATA_FIELD;
+import telran.net.exceptions.ServerUnavailableException;
 
 public class TcpClient implements Closeable {
     Socket socket;
@@ -43,6 +51,9 @@ public class TcpClient implements Closeable {
             }
 
         } while (count != 0);
+        if(socket == null) {
+            throw new ServerUnavailableException(host, port);
+        }
     }
 
     private void waitForInterval() {
@@ -52,12 +63,18 @@ public class TcpClient implements Closeable {
 
     @Override
     public void close() throws IOException {
-       socket.close();
+        if(socket != null) {
+            socket.close();
+        }
+       
     }
     public String sendAndReceive(String requestType, String requestData) {
         Request request = new Request(requestType, requestData);
        
         try {
+            if(socket == null) {
+                throw new ServerUnavailableException(host, port);
+            }
             writer.println(request);
             String responseJSON = reader.readLine();
             JSONObject jsonObj = new JSONObject(responseJSON);
@@ -68,7 +85,8 @@ public class TcpClient implements Closeable {
             }
             return responseData;
         } catch (IOException e) {
-            throw new RuntimeException("Server is unavailable");
+            connect();
+            throw new ServerUnavailableException(host, port);
         }
     }
 
